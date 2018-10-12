39d72529800f3bae773d7417c4d6b5dc4a2d7c3c
gonghao <1506301420@qq.com>
2018-10-12 11:59:40 +0800

    m

--- src/api/host.ts
+++ src/api/host.ts
@@ -2,14 +2,14 @@
  * @Author: gonghao
  * @Date: 2018-10-09 17:02:43
  * @Last Modified by: gonghao
- * @Last Modified time: 2018-10-09 18:51:11
+ * @Last Modified time: 2018-10-12 11:59:28
  * @Desc: 存放各个环境的host
  */
 import { Host } from '@/api/types';
 
 export const adamHost: Host = {
-  development: 'http://adam.backend.51ping.com',
-  beta: 'http://adam.backend.51ping.com',
-  product: 'http://adam.backend.sankuai.com',
+  development: '',
+  beta: '',
+  product: '',
 };
 
